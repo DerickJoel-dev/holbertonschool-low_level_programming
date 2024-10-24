b6c0f3a63ea6befcd473b9129e0b1a5b0afe690d
DerickJoel-dev <derickdev2@gmail.com>
2024-10-24 12:00:52 -0400

    last digit of variable n

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,15 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 
-
-/* betty style doc for function main goes there */
+/* betty style doc for function main goes there*/
 /**
- * main - Entry ponit
+ * main - Entry point
  *
  * Return: Always 0 (Success)
- *
  */
-
 int main(void)
 {
 	int n;
@@ -18,11 +15,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is positive\n", n);
+		printf("%i is positive\n", n);
 	else if (n == 0)
-		printf("%d is zero\n", n);
+		printf("%i is zero\n", n);
 	else if (n < 0)
-		printf("%d is negative\n", n);
+		printf("%i is negative\n", n);
 
 	return (0);
 }
