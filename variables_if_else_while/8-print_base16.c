ff885b85c35d2e9869463f1a5ff79d7fbeb9074d
DerickJoel-dev <derickdev2@gmail.com>
2024-10-24 20:09:20 -0400

    print numbers and letters

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always o
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar9'\n')
+		return (0);
+}
