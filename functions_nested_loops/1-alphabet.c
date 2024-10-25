1c275d92578c2d485f55df9ef7fcdbf85d04e40e
DerickJoel-dev <derickdev2@gmail.com>
2024-10-24 21:22:45 -0400

    program that print the alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry ponit.
+ *
+ * Description - Function that prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ *
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		_putchar(ch);
+	_putchar('\n');
+}
