e91a8528cf88b274aa6f55f161594344cfd500dd
DerickJoel-dev <derickdev2@gmail.com>
2024-11-03 15:26:53 -0400

    prints a string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
+
+void _puts(char *str)
+{
+
+	int n = 0;
+
+	while (str[n])
+	{
+		_putchar(str[n]);
+		++n;
+	}
+
+	_putchar('\n');
+}
