e90745ecdab586b85a5f748e015ca544d12aa3ce
DerickJoel-dev <derickdev2@gmail.com>
2024-11-09 14:26:27 -0400

    func that prints the cheessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,26 +1,11 @@
 #include "main.h"
-
 /**
-<<<<<<< HEAD
- * print_chessboard - print the chessboard
- * @a: pointer to 2D array
+ * print_chessboard - prints a chessboard
+ * @a: pointer
  *
+ * Return: no return
  */
-void print_chessboard(char (*a)[8])
-{
-	int row, col;
 
-	for (row = 0; row < 8; ++row)
-	{
-		for (col = 0; col < 8; ++col)
-			_putchar(a[row][col]);
-		_putchar('\n');
-	}
-=======
- * print_chessboard - prints the chessboard
- * @a: input pointer.
- * Return: no return.
- */
 void print_chessboard(char (*a)[8])
 {
 	unsigned int i, m = 0;
@@ -35,5 +20,4 @@ void print_chessboard(char (*a)[8])
 		_putchar(a[i / 8][i - m]);
 	}
 	_putchar('\n');
->>>>>>> 8eee7910d783129663e524bda80c203095b085e0
 }
