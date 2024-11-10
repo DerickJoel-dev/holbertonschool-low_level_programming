775d36889ed09261f3b5f5a5bb8fb3521c2425c0
DerickJoel-dev <derickdev2@gmail.com>
2024-11-10 15:03:13 -0400

    print its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints ots name, followed by a new line.
+ * @argc: the number of arguments supplied to the program.
+ * @argv: an array of pointer to the argument.
+ * Return: always 0.
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
