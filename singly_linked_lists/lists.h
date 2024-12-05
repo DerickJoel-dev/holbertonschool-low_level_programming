6ea8ab7efc370f410429343f91615169f873ecd4
DerickJoel-dev <derickdev2@gmail.com>
2024-12-05 17:17:34 -0400

    func prints all elements of a list

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,9 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/* ======= LIBRARIES ======= */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+/* ======= STRUCTURE ======= */
 
 /**
  * struct list_s - singly linked list
@@ -15,10 +19,12 @@
  */
 typedef struct list_s
 {
-  char *str;
-  unsigned int len;
-  struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
+
+/* ======== PROTOTYPES ======== */
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
