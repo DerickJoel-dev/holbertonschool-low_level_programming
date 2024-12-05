b0f53396edd352438e205b6ea9d73489465cc421
DerickJoel-dev <derickdev2@gmail.com>
2024-12-05 19:21:51 -0400

    func adds a new node at the end

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end
+ * of a list_t list.
+ * @head: the start of the list.
+ * @str: the string to be duplicated.
+ *
+ * Return: the address of the new element
+ * or NULL if it failed.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	char *string_duplicate;
+	int length;
+	list_t *new_node;
+	list_t *last;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	string_duplicate = strdup(str);
+	if (string_duplicate == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	for (length = 0; str[length];)
+		length++;
+
+	new_node->str = string_duplicate;
+	new_node->len = length;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new_node;
+	}
+	return (*head);
+}
